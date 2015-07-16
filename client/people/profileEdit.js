f363a608e4c3669070e994c3980559461835d910
danzakon <danzakon@gmail.com>
2015-07-16 15:22:25 -0400

    Added an age feature to edit profile

--- client/people/profileEdit.js
+++ client/people/profileEdit.js
@@ -11,11 +11,13 @@ Template.profileEdit.events({
 		var bio = event.target.bio.value;
 		var firstName = event.target.firstName.value;
 		var lastName = event.target.lastName.value;
+		var age = event.target.age.value;
 		var userName = event.target.userName.value;
 		Meteor.users.update(this._id,
 			{$set:{'profile.bio':bio, 
 					'profile.firstName':firstName, 
 					'profile.lastName':lastName,
+					'profile.age':age,
 					'profile.userName':userName}});
 		Router.go('/profile/'+this._id);
 		
