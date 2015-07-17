34322d873e0c1e18bc3ea439f412df04a478f47b
danzakon <danzakon@gmail.com>
2015-07-17 10:36:38 -0400

    Layout for search bar (not yet operating)

--- client/people/profile.js
+++ client/people/profile.js
@@ -10,3 +10,7 @@ Template.profile.helpers({
 	photo:function(){ // returns the URL of the gravatar photo for this email
 		return Gravatar.imageUrl(Gravatar.hash(this.emails[0].address,{secure:true}))}
 })
+
+
+
+
