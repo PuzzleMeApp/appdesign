02d6bc445936369a99cbc9391356a1444b39d08f
jplayer1127 <jordan@Jordans-MacBook-Pro.local>
2015-07-17 11:09:28 -0400

    fixed packages

--- client/puzzledisplay.js
+++ client/puzzledisplay.js
@@ -2,11 +2,11 @@ Template.puzzledisplay.events({
 'click #submitPuzzle': function(event){
 	var newUserAnswer = $("#newuseranswer").val();
 	
-	if (newUserAnswer == $(Session.get("answer"))) {
+	if (newUserAnswer == (Session.get("answer"))) {
 		console.log("hi");
 	}
 
-	$("#newuseranswer").val("");
+	
 
 
 
