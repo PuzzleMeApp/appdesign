2ffcc675cd675504e20f9b0ab7e7f822efb72960
jplayer1127 <jordan@Jordans-MacBook-Pro.local>
2015-07-17 15:55:37 -0400

    fixing search

--- client/people/URLPhotopull.js
+++ client/people/URLPhotopull.js
@@ -1,17 +1,9 @@
 Template.URLPhotopull.events({
 	"click #photoSubmit": function(event){
-		event.preventDefault();
-		Session.set("URL",$("#photoURL").val());
-
-		alert(URL);
+		var url = ({url:$("#photoURL").val()});
 
 		
-	}
-})
 
-Template.URLPhotopull.helpers({
-	
-	photo:function(){ // returns the URL of the gravatar photo for this email
-		return URL
+		
 	}
 })
