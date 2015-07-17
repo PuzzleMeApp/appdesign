f32e57f9fe25c6c6dd388fabe41d164843a57593
jplayer1127 <jordan@Jordans-MacBook-Pro.local>
2015-07-17 15:15:59 -0400

    A bunch of search stuff

--- client/people/profileEdit.js
+++ client/people/profileEdit.js
@@ -11,15 +11,28 @@ Template.profileEdit.events({
 		var bio = event.target.bio.value;
 		var firstName = event.target.firstName.value;
 		var lastName = event.target.lastName.value;
-		Session( "age",event.target.age.value);
-		var userName = event.target.userName.value;
+		Session.set( "age",event.target.age.value);
+		Session.set("userName",event.target.userName.value);
 		Meteor.users.update(this._id,
 			{$set:{'profile.bio':bio, 
 					'profile.firstName':firstName, 
 					'profile.lastName':lastName,
-					'profile.age':age,
-					'profile.userName':userName}});
+					'profile.age':Session.get("age"),
+					'profile.username':Session.get("userName")}});
 		Router.go('/profile/'+this._id);
+
+		var userData = {
+			bio:bio,
+			firstName:firstName,
+			lastName:lastName,
+			age:Session.get("age"),
+			userName:Session.get("userName"),
+			userID:this._id
+
+
+		}
+		People.insert(userData);
+
 		
 	}
 })
--- client/subscriptions.js
+++ client/subscriptions.js
@@ -1,3 +1,4 @@
 Meteor.subscribe("theProfiles");
 Meteor.subscribe("userData");
 Meteor.subscribe("ourpuzzles");
+Meteor.subscribe("theUsers");
