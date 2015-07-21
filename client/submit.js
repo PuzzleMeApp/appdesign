079cf0c83ca12452d01cfdf1d3388fcab7be17b3
NateDesign <nishantdate@gmail.com>
2015-07-21 11:22:08 -0400

    Image as a part of the DB

--- client/submit.js
+++ client/submit.js
@@ -5,13 +5,20 @@ Template.submit.events({
  	var newPuzzleName = $("#newpuzzlename").val();
  	var newPuzzle = $("#newpuzzle").val();
  	var newPuzzleAnswer = $("#newpuzzleanswer").val();
+<<<<<<< Updated upstream
  	
+=======
+ 	 Session.set("answer",$("#newpuzzleanswer").val());
+ 	var newPuzzleImage=$("#newpuzzleimage").val();
+
+>>>>>>> Stashed changes
  	
  	
 
  	$("#newpuzzlename").val("");
  	$("#newpuzzle").val("");
  	$("#newpuzzleanswer").val("");
+ 	$("#newpuzzleimage").val("");
 
  	
 
@@ -19,10 +26,17 @@ Template.submit.events({
  	var puzzledata =
  		{puzzlename:newPuzzleName.toLowerCase(),
  			puzzle:newPuzzle,
+<<<<<<< Updated upstream
  			puzzleanswer:newPuzzleAnswer.toLowerCase(),
  			createdAt:new Date(), 			
 			likes:0,
 			likers:[]
+=======
+ 			puzzleanswer:newPuzzleAnswer,
+ 			createdAt:new Date(),
+ 			image=newPuzzleImage,
+ 			counter:0,
+>>>>>>> Stashed changes
 
  	
  	};
